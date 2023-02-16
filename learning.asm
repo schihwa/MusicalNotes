6b773052b0aa9f739ed7d42cce158448be3080c5
schihwa <schihwa109@gmail.com>
2023-02-16 19:51:02 +0000

    added correct responses to changing background colour

--- learning.asm
+++ learning.asm
@@ -5,17 +5,19 @@ invalidNum: .asciiz "Invalid number entered"
 frameBuffer: .space 0x80000	#512 wide x 256 high pixels
 
 explain:    .asciiz     "Options:\tColours:\n1 - cls\t\t1 - red\n2 - stave\t2 - orange\n3 - note\t3 - yellow\n4 - exit\t4 - green\n\t\t5 - blue\n"
-red: .asciiz "Background coloured red!\n"
-orange: .asciiz "Background coloured orange!\n"
-yellow: .asciiz "Background coloured yellow!\n"
-green: .asciiz "Background coloured green!\n"
-blue: .asciiz "Background coloured blue!\n"
 
-colors: .word 0xFFA500, 0xFFFF00, 0x008000, 0x0000FF, 0xFF0000	
-# 			                 red	        orange      yellow	    green       blue        
+colors: 	.word 0xFFA500, 0xFFFF00, 0x008000, 0x0000FF, 0xFF0000	
+
+orange:  .asciiz "background colour set to Orange!\n"
+yellow:  .asciiz "background colour set to Yellow!\n"
+green:   .asciiz "background colour set to Green!\n"
+blue:    .asciiz "background colour set to Blue!\n"
+red:     .asciiz "background colour set to Red!\n"
+
+colorNames:
+         .word   orange, yellow, green, blue, red
 
 .text
-
 main:
 	jal options
     while: 
@@ -24,8 +26,11 @@ main:
         syscall
         move $t0, $v0 
         
+        # subtract 1 from the user input to match the index in the colors array
+	addi $t0, $t0, -1
+        
         #error handling numbers beyond 1-5
-        li $t1, 1
+        li $t1, 0
         li $t2, 5
         
         blt $t0, $t1, invalid # If the number is less than the minimum, branch to the invalid label
@@ -38,12 +43,24 @@ main:
         #jump to outside_bounds if number is outside the bounds
         j outside_bounds             
 
-            inside_bounds:
-            #this will change the background colour
-            la $t3, colors # load the address of the colors array
-            sll $t0, $t0, 2 # multiply the index by 4 to get the correct offset in the array
-            add $t3, $t3, $t0 # add the offset to the array address to get the address of the desired color
-            lw $a0, ($t3) # load the color value into $a0
+inside_bounds:
+    # load the address of the colors array
+    la $t3, colors
+    # multiply the index by 4 to get the correct offset in the array
+    sll $t0, $t0, 2
+    # add the offset to the array address to get the address of the desired color
+    add $t3, $t3, $t0
+    # load the color value into $a0
+    lw $a0, ($t3)
+
+
+    # load the address of the color names array
+    la $t4, colorNames
+    # add the index multiplied by the size of each element to the address to get the address of the desired color name
+    add $t4, $t4, $t0
+    # load the address of the color name into $a1
+    lw $a1, 0($t4)
+
             # call the backgroundColour function with the selected color
             jal backgroundColour
 
@@ -55,7 +72,6 @@ main:
     exit:
         li $v0, 10             # Set system call code for exiting the program
         syscall                # Call the system call to exit the program
-
 
 invalid:
     li $v0, 4 # Print the invalid number message and exit
@@ -79,17 +95,17 @@ backgroundColour:
         addi $t1, $t1, 1     # increment the loop counter
         addi $s0, $s0, 4     # increment the memory address pointer by 1 byte (the size of a pixel)
         bne $t1, 131072, LoopR # loop until the counter reaches 25600 (the number of pixels in the bitmap)
-
-        #print string
+        
+        #print colour
         addi $v0,$zero,4		#put syscall service into v0
-        la $a0,	red		#put address of string (input) into a0
+        la $a0,	($a1)		#put address of string (input) into a0
         syscall				#actually print string! (this works!)
         jr $ra 
 	
 options:
 	#print string
 	addi $v0,$zero,4		#put syscall service into v0
-	la $a0, explain			#put address of string (input) into a0
+	la  $a0, explain			#put address of string (input) into a0
 	syscall				#actually print string! (this works!)
 	jr $ra 
 
